6e30337ab040d0f077181273b7c519f1a17c869a
Sirvent94 <sirvent94@gmail.com>
2019-03-11 01:54:01 -0600

    Second Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import DogMeme from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<DogMeme />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+class DogMeme extends React.Component{
+
+	constructor(props){
+		super(props);
+		this.state={topval:'Yei', botval: 'WOW'};
+
+	}
+
+	render(){
+		return(
+			
+				
+
+			<div className="info">
+				<div className="row">
+
+				<figure>
+					<form>
+						<h1 className="h1">Create</h1>
+						<h1 className="h1">Meme</h1>
+
+					</form>
+				</figure>
+				</div>
+			</div>
+			);
+	}
+}
+
+ReactDOM.render(<DogMeme/>, document.getElementById('root'));
